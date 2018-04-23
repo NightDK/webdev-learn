5487e008a0c8601f518c815950e20df2a6483583
NightDK <mishail-99@mail.ru>
2018-04-23 19:23:52 +0300

    Начало lw6

---
+++ lw6/js/coffeeScript.js
@@ -0,0 +1,9 @@
+function CoffeeMachine() {
+    this.moneyCash;
+    this.coffeeMenu = [
+      { id: 'Американо', price: 10 },
+      { id: 'Латте', price: 15 },
+      { id: 'Каппучино', price: 20 },
+    ];
+    this.currentCoffeeNumber;
+}
