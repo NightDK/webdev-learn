c23f47e76b0800be847fa024ec447fe462a415de
NightDK <mishail-99@mail.ru>
2018-04-23 19:34:52 +0300

    вывод меню

--- lw6/js/coffeeScript.js
+++ lw6/js/coffeeScript.js
@@ -18,3 +18,12 @@ function CoffeeMachine() {
     }
     return false;
   }
+
+  this.printCoffeeMenu = function () {
+    let menu = ' ';
+    for (i in this.coffeeMenu) {
+      menu += Number(i) + 1 + ' ' + this.coffeeMenu[i].price + 'Руб. ' + this.coffeeMenu[i].id + '\n';
+    }
+    console.log(menu);
+    return menu;
+  }
