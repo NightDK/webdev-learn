45a870f8e87a12597211c85e93a7d468a1e0ecc9
NightDK <mishail-99@mail.ru>
2018-04-25 15:17:25 +0300

    финальные изменения

--- lw6/js/coffeeScript.js
+++ lw6/js/coffeeScript.js
@@ -1,14 +1,14 @@
 function CoffeeMachine() {
-  this.moneyCash;
+  this.moneyCash = 0;
   this.coffeeMenu = [
-    { id: 'Американо', price: 10 },
-    { id: 'Латте', price: 15 },
-    { id: 'Каппучино', price: 20 },
+    { name: 'Американо', price: 10 },
+    { name: 'Латте', price: 15 },
+    {name: 'Каппучино', price: 20 },
   ];
   this.selectCoffeeNumber;
 
   this.setCash = function (money) {
-    if (typeof money == 'number') {
+    if (typeof money === 'number') {
       if (money <= 0) {
         console.log('error: money <= 0');
         return false;
@@ -22,16 +22,16 @@ function CoffeeMachine() {
   this.printCoffeeMenu = function () {
     let menu = ' ';
     for (i in this.coffeeMenu) {
-      menu += Number(i) + 1 + ' ' + this.coffeeMenu[i].price + 'Руб. ' + this.coffeeMenu[i].id + '\n';
+      menu += Number(i) + 1 + ' ' + this.coffeeMenu[i].price + 'Руб. ' + this.coffeeMenu[i].name + '\n';
     }
     console.log(menu);
     return menu;
   }
 
   this.chooseCoffee = function (coffeeNumber) {
-    if (typeof coffeeNumber == 'number') {
-      if (coffeeNumber >= 1 && coffeeNumber < this.coffeeNumber.length + 1) {
-        if (this.moneyCash >= this.coffeeNumber[coffeeNumber - 1].price){
+    if (typeof coffeeNumber === 'number') {
+      if (coffeeNumber >= 1 && coffeeNumber <= 3) {
+        if (this.moneyCash >= this.coffeeMenu[coffeeNumber - 1].price){
           console.log('choosed ' + coffeeNumber);
           this.selectCoffeeNumber = coffeeNumber - 1;
           return true;
@@ -45,8 +45,8 @@ function CoffeeMachine() {
     return false;
   }
 
-  this.getRemainCash = function () {
-    if (typeof this.selectCoffeeNumber == 'number' && typeof this.moneyCash == 'number'){
+  this.returnCash = function () {
+    if (typeof this.selectCoffeeNumber === 'number' && typeof this.moneyCash === 'number'){
       backMoney = this.moneyCash - this.coffeeMenu[this.selectCoffeeNumber].price;
       console.log('Ваша сдача: ' + backMoney);
       return backMoney;
