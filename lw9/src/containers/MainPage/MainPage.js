dac1d8005d0caada1d82dcfc5f23dc2513131b7a
NightDK <mishail-99@mail.ru>
2018-05-10 17:34:44 +0300

    работа №9

---
+++ lw9/src/containers/MainPage/MainPage.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react'
+import { Container, ListGroup, Badge } from 'reactstrap'
+import './MainPage.css'
+import Product from '../../components/Product/Product'
+import Error from '../../components/Error/Error'
+import PreloaderIcon from 'react-preloader-icon'
+import Spinning from 'react-preloader-icon/loaders/Spinning'
+import { API } from '../../constants/index'
+
+class MainPage extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      error: null,
+      isLoaded: false,
+      products: []
+    }
+  }
+
+  componentDidMount() {
+    fetch(API)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true
+          })
+          if (!Array.isArray(result)) {
+            this.setState({ error: result })
+          } else {
+            this.setState({
+              products: result
+            })
+          }
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          })
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, products } = this.state
+    if (error !== null) {
+      return <Error error={JSON.stringify(error)} />
+    } else if (!isLoaded) {
+      return (
+        <Container>
+          <PreloaderIcon
+            className='preloader'
+            loader={Spinning}
+            size={100}
+            strokeWidth={8}
+            strokeColor="#000000"
+            duration={800}
+          />
+        </Container>
+      )
+    }
+    return (
+      <Container>
+        <h1><Badge color='primary'>Products:</Badge></h1>
+        <ListGroup>
+          {
+            products.map(product => (
+              <Product
+                productName={product.name}
+                key={product._id}
+                link={`/reviews/${product._id}`}
+              />
+            ))
+          }
+        </ListGroup>
+      </Container>
+    )
+  }
+}
+
+export default MainPage
---
+++ lw9/src/containers/ReviewsPage/ReviewsPage.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react'
+import { Container, ListGroup, Badge } from 'reactstrap'
+import './ReviewsPage.css'
+import Review from '../../components/Review/Review'
+import Error from '../../components/Error/Error'
+import PreloaderIcon from 'react-preloader-icon'
+import Spinning from 'react-preloader-icon/loaders/Spinning'
+import { API } from '../../constants/index'
+
+class ReviewsPage extends Component {
+  constructor(props) {
+    super(props)
+    this.productId = this.props.match.params.id
+    this.state = {
+      error: null,
+      isLoaded: false,
+      reviews: []
+    }
+  }
+
+  componentDidMount() {
+    fetch(`${API}/${this.productId}/reviews`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true
+          })
+          if (!Array.isArray(result)) {
+            this.setState({ error: result })
+          } else {
+            this.setState({
+              reviews: result
+            })
+          }
+        },
+        (error) => {
+          console.log(error)
+          this.setState({
+            isLoaded: true,
+            error: error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, reviews } = this.state
+    if (error !== null) {
+      return <Error error={JSON.stringify(error)} />
+    } else if (!isLoaded) {
+      return (
+        <Container>
+          <PreloaderIcon
+            className='preloader'
+            loader={Spinning}
+            size={100}
+            strokeWidth={8}
+            strokeColor="#000000"
+            duration={800}
+          />
+        </Container>
+      )
+    } else {
+      return (
+        <Container>
+          <h1><Badge color='primary'>Reviews:</Badge></h1>
+          <ListGroup>
+            {
+              reviews.map(review => (
+                <Review
+                  review={review.discription}
+                  key={review._id}
+                />
+              ))
+            }
+          </ListGroup>
+        </Container>
+      )
+    }
+  }
+}
+
+export default ReviewsPage
